fe3720bb566831775570a726846fde66b3be7fce
Scott Salisbury <scottsalisbury@gmail.com>
2016-04-05 23:36:05 +0100

    Updates to message text.

--- remote-uploads.sh
+++ remote-uploads.sh
@@ -21,14 +21,18 @@ mkdir -p $CONFIG$APP_DOCROOT$APP_UPLOADS
 if [[ $1 = "enable" ]]; then
 
 	if [[ $APP_SSL = "true" ]]; then
+		echo Creating file: $APP_DOCROOT$APP_UPLOADS/.htaccess
 		sed -e 's#__REPLACE__#'https://$DOMAIN_REMOTE'#g' $CONFIG/vendor/scottjs/helper-scripts/.htaccess-sample > $CONFIG$APP_DOCROOT$APP_UPLOADS/.htaccess
 		echo COMPLETE: Loading remote uploads now enabled: https://$DOMAIN_REMOTE$APP_UPLOADS
 	else
+		echo Creating file: $APP_DOCROOT$APP_UPLOADS/.htaccess
 		sed -e 's#__REPLACE__#'http://$DOMAIN_REMOTE'#g' $CONFIG/vendor/scottjs/helper-scripts/.htaccess-sample > $CONFIG$APP_DOCROOT$APP_UPLOADS/.htaccess
 		echo COMPLETE: Loading remote uploads now enabled: http://$DOMAIN_REMOTE$APP_UPLOADS
 	fi
 
 elif [[ $1 = "disable" ]]; then
+
+	echo Removing file: $APP_DOCROOT$APP_UPLOADS/.htaccess
 
 	rm -rf "$CONFIG$APP_DOCROOT$APP_UPLOADS/.htaccess"
 
